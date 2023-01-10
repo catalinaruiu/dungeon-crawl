7f9f408803c10ee41949dc94acb41ddb3ede20fb
alex <bornazalexg@gmail.com>
2023-01-10 15:25:37 +0200

    Cell types added

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -3,8 +3,10 @@ package com.codecool.dungeoncrawl.logic;
 public enum CellType {
     EMPTY("empty"),
     FLOOR("floor"),
-    WALL("wall");
-
+    WALL("wall"),
+    OPEN_DOOR("openDoor"),
+    CLOSED_DOOR("closedDoor"),
+    STAIRS("stairs");
     private final String tileName;
 
     CellType(String tileName) {
