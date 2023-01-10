ec3837a6d4225f12dc16eefabc5bef62115f6159
alex <bornazalexg@gmail.com>
2023-01-10 15:29:47 +0200

    MapLoader methods added

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -1,14 +1,24 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.actors.Nazgul;
+import com.codecool.dungeoncrawl.logic.actors.Ork;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import com.codecool.dungeoncrawl.logic.items.Key;
+import com.codecool.dungeoncrawl.logic.items.Sword;
+import com.codecool.dungeoncrawl.logic.items.door.ClosedDoor;
 
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class MapLoader {
-    public static GameMap loadMap() {
-        InputStream is = MapLoader.class.getResourceAsStream("/map.txt");
+    public static GameMap loadMap(int playerOnMap) {
+        InputStream is;
+        if (playerOnMap == 1) {
+            is = MapLoader.class.getResourceAsStream("/map.txt");
+        } else {
+            is = MapLoader.class.getResourceAsStream("/map2.txt");
+        }
         Scanner scanner = new Scanner(is);
         int width = scanner.nextInt();
         int height = scanner.nextInt();
@@ -38,6 +48,31 @@ public class MapLoader {
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
+                            break;
+                        case 'k':
+                            cell.setType(CellType.FLOOR);
+                            new Key(cell);
+                            break;
+                        case 'w':
+                            cell.setType(CellType.FLOOR);
+                            new Sword(cell);
+                            break;
+                        case 'o':
+                            cell.setType(CellType.FLOOR);
+                            new Ork(cell);
+                            map.addMonsterToMap(new Ork(cell));
+                            break;
+                        case 'n':
+                            cell.setType(CellType.FLOOR);
+                            new Nazgul(cell);
+                            map.addMonsterToMap(new Nazgul(cell));
+                            break;
+                        case 'd':
+                            cell.setType(CellType.CLOSED_DOOR);
+                            new ClosedDoor(cell);
+                            break;
+                        case 'r':
+                            cell.setType(CellType.STAIRS);
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
