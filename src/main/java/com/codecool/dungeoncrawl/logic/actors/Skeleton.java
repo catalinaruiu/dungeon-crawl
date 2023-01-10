42b3cb127f4333b85b9a840d48d29c577f572255
alex <bornazalexg@gmail.com>
2023-01-10 14:05:46 +0200

    Skeleton monster modified

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -5,6 +5,8 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+        this.setHealth(5);
+        this.setStr(1);
     }
 
     @Override
