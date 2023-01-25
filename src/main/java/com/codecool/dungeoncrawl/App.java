9f4730f6e04399261d766599d7b1e97c1c244158
catalinaruiu <catalinaruiu@yahoo.com>
2023-01-25 23:41:58 +0200

    connected database

--- src/main/java/com/codecool/dungeoncrawl/App.java
+++ src/main/java/com/codecool/dungeoncrawl/App.java
@@ -1,7 +1,17 @@
 package com.codecool.dungeoncrawl;
 
+import com.codecool.dungeoncrawl.dao.GameDatabaseManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) {
-        Main.main(args);
+        //Main.main(args);
+        GameDatabaseManager gameDatabaseManager = new GameDatabaseManager();
+        try{
+            gameDatabaseManager.setup();
+        }
+        catch (SQLException e){
+            System.out.println("database not found");
+        }
     }
 }
